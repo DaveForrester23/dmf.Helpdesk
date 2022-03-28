8a2fc00747e7ff511a0999bae7d70caaee905d81
DaveForrester23 <46917938+DaveForrester23@users.noreply.github.com>
2022-03-28 11:31:27 +0100

    Add project files.

---
+++ Controllers/TicketController.cs
@@ -0,0 +1,35 @@
+﻿using dmf.Helpdesk.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dmf.Helpdesk.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TicketController> _logger;
+
+        public TicketController(ILogger<TicketController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<TicketModel> Get()
+        {
+            var tickets = new List<TicketModel>();
+
+            return tickets.ToArray();
+        }
+    }
+}
---
+++ Models/TicketModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dmf.Helpdesk.Models
+{
+    public class TicketModel
+    {
+        public string TicketNo
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateCreated
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateModified
+        {
+            get;
+            set;
+        }
+
+        public string RequesterUsername
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
